adc33d97c918bd557d62ed9c0f9231a9854389b5
rodkristian <krinkline@gmail.com>
2024-02-21 17:06:42 +0800

    added js

---
+++ miniproject1/assets/js/app.js
@@ -0,0 +1,11 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-bar").style.top = "0";
+  } else {
+    document.getElementById("nav-bar").style.top = "-80px";
+    document.getElementById("nav-bar").style.color = red;
+  }
+  prevScrollpos = currentScrollPos;
+}
