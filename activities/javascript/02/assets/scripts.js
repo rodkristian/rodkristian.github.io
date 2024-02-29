44ef79af28e9c9aaeb41035e468e24e8f7ef6a42
rodkristian <krinkline@gmail.com>
2024-02-29 22:05:53 +0800

    added 02

---
+++ activities/javascript/02/assets/scripts.js
@@ -0,0 +1,43 @@
+let english = document.getElementById("inp-eng");
+let math = document.getElementById("inp-math");
+let datastructure = document.getElementById("inp-datastruct");
+let compprogramming = document.getElementById("inp-comprog");
+let webprogramming = document.getElementById("inp-webprog");
+
+// let tot = parseInt(english) + parseInt(math) + parseInt(datastructure) + parseInt(compprogramming) + parseInt(webprogramming);
+
+english.addEventListener('keyup', function() {
+    let total = parseFloat(english.value) + parseFloat(math.value) + parseFloat(datastructure.value) + parseFloat(compprogramming.value) + parseFloat(webprogramming.value);
+    document.getElementById("tot-grade").innerText = total;
+    let average = total / 5;
+    document.getElementById("ave").innerText = average;
+});
+
+math.addEventListener('keyup', function() {
+    let total = parseFloat(english.value) + parseFloat(math.value) + parseFloat(datastructure.value) + parseFloat(compprogramming.value) + parseFloat(webprogramming.value);
+    document.getElementById("tot-grade").innerText = total;
+    let average = total / 5;
+    document.getElementById("ave").innerText = average;
+});
+
+datastructure.addEventListener('keyup', function() {
+    let total = parseFloat(english.value) + parseFloat(math.value) + parseFloat(datastructure.value) + parseFloat(compprogramming.value) + parseFloat(webprogramming.value);
+    document.getElementById("tot-grade").innerText = total;
+    let average = total / 5;
+    document.getElementById("ave").innerText = average;
+});
+
+compprogramming.addEventListener('keyup', function() {
+    let total = parseFloat(english.value) + parseFloat(math.value) + parseFloat(datastructure.value) + parseFloat(compprogramming.value) + parseFloat(webprogramming.value);
+    document.getElementById("tot-grade").innerText = total;
+    let average = total / 5;
+    document.getElementById("ave").innerText = average;
+});
+
+webprogramming.addEventListener('keyup', function() {
+    let total = parseFloat(english.value) + parseFloat(math.value) + parseFloat(datastructure.value) + parseFloat(compprogramming.value) + parseFloat(webprogramming.value);
+    document.getElementById("tot-grade").innerText = total;
+    let average = total / 5;
+    document.getElementById("ave").innerText = average;
+});
+
---
+++ activities/javascript/02/readme.md
@@ -0,0 +1,11 @@
+# Simple Grading System
+
+A simple application to get the total grades and the average.
+
+## Instructions
+
+```bash
+    Add JavaScript Command/Syntax compute the grade dynamically.
+
+    Use change function for automatically compute the grade upon value change.
+```
